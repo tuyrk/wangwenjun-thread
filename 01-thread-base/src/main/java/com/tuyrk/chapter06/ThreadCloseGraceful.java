4ce12d022fed94080678ce3a546da4e60f04a3e5
tuyrk <tuyrk@qq.com>
2019-11-23 21:40:21 +0800

    添加类说明注释

---
+++ 01-thread-base/src/main/java/com/tuyrk/chapter06/ThreadCloseGraceful.java
@@ -0,0 +1,31 @@
+package com.tuyrk.chapter06;
+
+/**
+ * 优雅地停止线程 Graceful thread stop
+ * 1. 使用开关变量控制是否启动
+ */
+public class ThreadCloseGraceful {
+    private static class Worker extends Thread {
+        private volatile boolean start = true;
+
+        @Override
+        public void run() {
+            while (start) {
+                // ...
+            }
+        }
+
+        public void shutdown() {
+            this.start = false;
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Worker worker = new Worker();
+        worker.start();
+
+        Thread.sleep(10_000);
+
+        worker.shutdown();
+    }
+}
