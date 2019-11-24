e3f97443c6c5b0abc779313dfd51b209f9b2a8a8
tuyrk <tuyrk@qq.com>
2019-11-24 18:02:14 +0800

    结合jconsole,jstack以及汇编指令认识synchronized关键字

---
+++ 01-thread-base/src/main/java/com/tuyrk/chapter07/SynchronizedTest.java
@@ -0,0 +1,33 @@
+package com.tuyrk.chapter07;
+
+/**
+ * synchronized关键字到底是什么？
+ *
+ * @author tuyrk
+ */
+public class SynchronizedTest {
+    // 代码规范：final命名的变量需要使用大写
+    private static final Object LOCK = new Object();
+
+    public static void main(String[] args) {
+
+        // 任务
+        Runnable runnable = () -> {
+            synchronized (LOCK) {
+                try {
+                    Thread.sleep(500_000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        // 创建并启动线程
+        Thread t1 = new Thread(runnable);
+        Thread t2 = new Thread(runnable);
+        Thread t3 = new Thread(runnable);
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
