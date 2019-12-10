aed9c2230f57d3878ec5d7f484526ab7ccce61b2
tuyrk <tuyrk@qq.com>
2019-12-10 22:36:43 +0800

    自定义个简单的线程池并且测试

---
+++ 01-thread-base/src/main/java/com/tuyrk/chapter13/SimpleThreadPool.java
@@ -0,0 +1,151 @@
+package com.tuyrk.chapter13;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
+/**
+ * 自定义简单线程池
+ *
+ * @author tuyrk
+ */
+public class SimpleThreadPool {
+    /**
+     * 传参线程池大小
+     */
+    private final int size;
+
+    /**
+     * 默认线程池大小
+     */
+    private static final int DEFAULT_SIZE = 10;
+
+    /**
+     * 线程名自增序号
+     */
+    private static AtomicInteger sequence = new AtomicInteger();
+
+    /**
+     * 线程名前缀
+     */
+    private static final String THREAD_PREFIX = "SIMPLE_THREAD_POOL-";
+
+    /**
+     * 线程组
+     */
+    private static final ThreadGroup GROUP = new ThreadGroup("Pool_Group");
+
+    /**
+     * 任务队列
+     */
+    private static final LinkedList<Runnable> TASK_QUEUE = new LinkedList<>();
+
+    /**
+     * 线程队列
+     */
+    private static final List<WorkerTask> THREAD_QUEUE = new ArrayList<>();
+
+    public SimpleThreadPool() {
+        this(DEFAULT_SIZE);
+    }
+
+    public SimpleThreadPool(int size) {
+        this.size = size;
+        init();
+    }
+
+    private void init() {
+        for (int i = 0; i < size; i++) {
+            createWorkTask();
+        }
+    }
+
+    private void createWorkTask() {
+        WorkerTask task = new WorkerTask(GROUP, THREAD_PREFIX + (sequence.getAndIncrement()));
+        task.start();
+        THREAD_QUEUE.add(task);
+    }
+
+    /**
+     * 提交任务
+     *
+     * @param runnable 任务
+     */
+    public void submit(Runnable runnable) {
+        synchronized (TASK_QUEUE) {
+            TASK_QUEUE.addLast(runnable);
+            TASK_QUEUE.notifyAll();
+        }
+    }
+
+    /**
+     * 封装任务类
+     */
+    private static class WorkerTask extends Thread {
+        private volatile TaskState taskState = TaskState.FREE;
+
+        public TaskState getTaskState() {
+            return this.taskState;
+        }
+
+        public WorkerTask(ThreadGroup group, String name) {
+            super(group, name);
+        }
+
+        @Override
+        public void run() {
+            OUTER:
+            while (this.taskState != TaskState.DEAD) {
+                Runnable runnable;
+                synchronized (TASK_QUEUE) {
+                    // 任务队列为空，则进入阻塞状态
+                    while (TASK_QUEUE.isEmpty()) {
+                        try {
+                            this.taskState = TaskState.BLOCKED;
+                            TASK_QUEUE.wait();
+                        } catch (InterruptedException e) {
+                            break OUTER;
+                        }
+                    }
+                    // 任务队列不为空，取出任务
+                    runnable = TASK_QUEUE.removeFirst();
+                }
+                // 任务不为空，则执行任务
+                if (runnable != null) {
+                    this.taskState = TaskState.RUNNING;
+                    runnable.run();
+                    this.taskState = TaskState.FREE;
+                }
+
+            }
+        }
+
+        public void close() {
+            this.taskState = TaskState.DEAD;
+        }
+    }
+
+    /**
+     * 线程状态
+     */
+    private enum TaskState {
+        FREE, RUNNING, BLOCKED, DEAD
+    }
+
+    public static void main(String[] args) {
+        SimpleThreadPool threadPool = new SimpleThreadPool();
+        IntStream.rangeClosed(1, 40).forEach(i -> {
+            threadPool.submit(() -> {
+                System.out.printf("The runnable %d be serviced by %s start.\n", i, Thread.currentThread().getName());
+                try {
+                    Thread.sleep(10_000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.printf("The runnable %d be serviced by %s finished.\n", i, Thread.currentThread().getName());
+            });
+        });
+    }
+}
