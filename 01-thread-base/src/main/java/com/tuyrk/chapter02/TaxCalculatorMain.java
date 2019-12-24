5f535049dd93dd76ef682aab164b549f7e89edc9
tuyrk <tuyrk@qq.com>
2019-12-24 18:09:47 +0800

    税收计算器。策略模式

--- 01-thread-base/src/main/java/com/tuyrk/chapter02/TaxCalculatorMain.java
+++ 01-thread-base/src/main/java/com/tuyrk/chapter02/TaxCalculatorMain.java
@@ -1,29 +1,51 @@
 package com.tuyrk.chapter02;
 
+import org.junit.Test;
+
 /**
  * 计算税率：工资*0.1 + 奖金*0.15
  */
 public class TaxCalculatorMain {
-    public static void main(String[] args) {
-        // 1. 模版方法
-        /*TaxCalculator calculator = new TaxCalculator(10000d, 2000d) {
+    /**
+     * 1. 模版方法
+     */
+    @Test
+    public void test1() {
+        TaxCalculator calculator = new TaxCalculator(10000d, 2000d) {
             @Override
             protected double calcTax() {
                 return getSalary() * 0.1 + getBonus() * 0.15;
             }
         };
-        System.out.println(calculator.calculate());*/
+        System.out.println(calculator.calculate());
+    }
 
-        // 2.1 策略模式
-        /*TaxCalculator calculator = new TaxCalculator(10000d, 2000d);
+    /**
+     * 2.1 策略模式
+     */
+    @Test
+    public void test2() {
+        TaxCalculator calculator = new TaxCalculator(10000d, 2000d);
         CalculatorStrategy strategy = new SimpleCalculatorStrategy();
         calculator.setCalculatorStrategy(strategy);
-        System.out.println(calculator.calculate());*/
+        System.out.println(calculator.calculate());
+    }
 
-        // 2.2 策略模式-Java8Lambda
-        /*TaxCalculator calculator = new TaxCalculator(10000d, 2000d);
+    /**
+     * 2.2 策略模式-Java8Lambda-1
+     */
+    @Test
+    public void test3() {
+        TaxCalculator calculator = new TaxCalculator(10000d, 2000d);
         calculator.setCalculatorStrategy((s, b) -> s * 0.1 + b * 0.15);
-        System.out.println(calculator.calculate());*/
+        System.out.println(calculator.calculate());
+    }
+
+    /**
+     * 2.2 策略模式-Java8Lambda-2
+     */
+    @Test
+    public void test4() {
         TaxCalculator calculator = new TaxCalculator(10000d, 2000d, (s, b) -> s * 0.1 + b * 0.15);
         System.out.println(calculator.calculate());
     }
